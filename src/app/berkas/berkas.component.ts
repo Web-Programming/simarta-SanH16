1872e86e5ce743e160a6a030dd26e826cc3382b8
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2022-11-18 20:20:00 +0700

    SIMARTA Part 4
    Lanjutan

--- src/app/berkas/berkas.component.ts
+++ src/app/berkas/berkas.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+//Buat Class Model untuk data Berkas
+export class Berkas {
+  'nama_berkas': string;
+  'no_berkas': any; //number / string
+  'jenis_file': string;
+  'pemilik': string[];
+
+}
+
 @Component({
   selector: 'app-berkas',
   templateUrl: './berkas.component.html',
@@ -8,6 +17,27 @@ import { Component, OnInit } from '@angular/core';
 export class BerkasComponent implements OnInit {
 
   constructor() { }
+
+  listBerkas : Berkas[] = [
+    {
+      nama_berkas: 'Dokumentasi Kegiatan',
+      no_berkas: 'B101',
+      jenis_file: '.doc',
+      pemilik: ['Adhitya Hasan', 'Sunny']
+    },
+    {
+      nama_berkas: 'Perancangan Web',
+      no_berkas: 'B102',
+      jenis_file: '.pdf',
+      pemilik: ['James Alexander']
+    },
+    {
+      nama_berkas: 'Penelitian COVID-19',
+      no_berkas: 'B103',
+      jenis_file: '.txt',
+      pemilik: ['Ahmad Farisi', 'Nur Rachmat']
+    },
+  ]
 
   ngOnInit(): void {
   }
--- src/app/surat/surat.component.ts
+++ src/app/surat/surat.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+//Buat Class Model untuk data Surat
+export class Surat {
+  '_id': string;
+  'nomor_surat': string;
+  'tgl_surat': any; //number / string
+  'jenis_surat': string;
+  'uraian': string;
+  'pejabat_penandatangan': string;
+  'nama_penandatangan': string;
+  'nama_ditugaskan': string[];
+
+}
+
 @Component({
   selector: 'app-surat',
   templateUrl: './surat.component.html',
@@ -8,6 +21,39 @@ import { Component, OnInit } from '@angular/core';
 export class SuratComponent implements OnInit {
 
   constructor() { }
+
+  listSurat : Surat[] = [
+    {
+      _id: '1',
+      nomor_surat: '001001',
+      tgl_surat: '2022-11-01',
+      jenis_surat: 'Surat Tugas',
+      uraian: 'ini adalah uraian',
+      pejabat_penandatangan: 'Kaprodi SI',
+      nama_penandatangan: 'Iis Pradesan',
+      nama_ditugaskan: ['Nur Rachmat']
+    },
+    {
+      _id: '2',
+      nomor_surat: '001002',
+      tgl_surat: '2022-11-02',
+      jenis_surat: 'Surat Keputusan',
+      uraian: 'ini adalah uraian',
+      pejabat_penandatangan: 'Kaprodi SI',
+      nama_penandatangan: 'Iis Pradesan',
+      nama_ditugaskan: ['Nur Rachmat', 'Yohannes']
+    },
+    {
+      _id: '3',
+      nomor_surat: '001003',
+      tgl_surat: '2022-11-03',
+      jenis_surat: 'Surat Probadi',
+      uraian: 'ini adalah uraian',
+      pejabat_penandatangan: 'Kaprodi SI',
+      nama_penandatangan: 'Iis Pradesan',
+      nama_ditugaskan: ['Nur Rachmat', 'Fransiska']
+    },
+  ]
 
   ngOnInit(): void {
   }
